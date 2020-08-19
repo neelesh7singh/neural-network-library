a000afc045732fa636f55cbfec19c1289f71585d
Neelesh <neeleshsingh767@gmail.com>
2020-08-19 22:53:04 +0530

    finished this lib and tested tooooo

--- Matrix.js
+++ Matrix.js
@@ -20,6 +20,13 @@ class Matrix {
       }
     }
   }
+  hmultiply(arg) {
+    for (let i = 0; i < this.rows; i++) {
+      for (let j = 0; j < this.cols; j++) {
+        this.data[i][j] *= arg.data[i][j];
+      }
+    }
+  }
   multiply(arg) {
     if (arg instanceof Matrix) {
       if (arg.rows !== this.cols) {
@@ -85,6 +92,39 @@ class Matrix {
     let m = new Matrix(arr.length, 1);
     for (let i = 0; i < arr.length; i++) m.data[i][0] = arr[i];
     return m;
+  }
+  static subtract(a, b) {
+    let result = new Matrix(a.rows, a.cols);
+    for (let i = 0; i < a.rows; i++) {
+      for (let j = 0; j < a.cols; j++) {
+        result.data[i][j] = a.data[i][j] - b.data[i][j];
+      }
+    }
+    return result;
+  }
+  static map(matrix, func) {
+    let m = new Matrix(matrix.rows, matrix.cols);
+    for (let i = 0; i < matrix.rows; i++) {
+      for (let j = 0; j < matrix.cols; j++) {
+        m.data[i][j] = func(matrix.data[i][j]);
+      }
+    }
+    return m;
+  }
+  static multiply(a, b) {
+    if (b.rows !== a.cols) {
+      console.log('Cols and rows did not match');
+      return undefined;
+    }
+    let result = new Matrix(a.rows, b.cols);
+    for (let i = 0; i < result.rows; i++) {
+      for (let j = 0; j < result.cols; j++) {
+        for (let k = 0; k < a.cols; k++) {
+          result.data[i][j] += a.data[i][k] * b.data[k][j];
+        }
+      }
+    }
+    return result;
   }
 }
 
--- neuralNetwork.js
+++ neuralNetwork.js
@@ -1,12 +1,18 @@
 function activationFunction(x) {
   return 1 / (1 + Math.exp(-x));
 }
+
+function dsigmoid(x) {
+  return x * (1 - x);
+}
+
 class NeuralNetwork {
   constructor(input_nurons, hidden_nurons, output_nurons) {
     this.input_nurons = input_nurons;
     this.hidden_nurons = hidden_nurons;
     this.hidden_layers = hidden_nurons.length;
     this.output_nurons = output_nurons;
+    this.learning_rate = 0.1;
     this.weights_ih = new Matrix(this.hidden_nurons[0], this.input_nurons);
     this.weights_ih.randomize();
 
@@ -46,7 +52,79 @@ class NeuralNetwork {
     }
     return hidden_output.toArray();
   }
+  train(inputs_array, targets) {
+    let ho = [];
+    let inputs = Matrix.fromArray(inputs_array);
+    let hidden_output = this.weights_ih.multiply(inputs);
+    hidden_output.add(this.biase_i);
+    hidden_output.map(activationFunction);
+    ho.push(hidden_output);
+    for (let i = 0; i < this.hidden_layers; i++) {
+      hidden_output = this.weightsHidden[i].multiply(hidden_output);
+      hidden_output.add(this.biase_h[i]);
+      hidden_output.map(activationFunction);
+      ho.push(hidden_output);
+    }
+    let outputs = hidden_output;
+    ho.pop();
+
+    targets = Matrix.fromArray(targets);
+
+    let weights = [this.weights_ih, ...this.weightsHidden];
+
+    let hoCount = 0;
+    let wtrans;
+    let error;
+    let gradient;
+    let trans;
+    let delta;
+    for (let i = weights.length - 1; i >= 0; i--) {
+      if (i === weights.length - 1) {
+        error = Matrix.subtract(targets, outputs);
+        gradient = Matrix.map(outputs, dsigmoid);
+        gradient.hmultiply(error);
+        gradient.multiply(this.learning_rate);
+        trans = Matrix.transpose(ho[ho.length - 1]);
+        delta = Matrix.multiply(gradient, trans);
+        // update
+        this.weightsHidden[this.hidden_layers - 1].add(delta);
+        this.biase_h[this.hidden_layers - 1].add(gradient);
+        weights[i].add(delta);
+      } else if (i === 0) {
+        wtrans = Matrix.transpose(weights[i + 1]);
+        error = Matrix.multiply(wtrans, error);
+        gradient = Matrix.map(ho[0], dsigmoid);
+        gradient.hmultiply(error);
+        gradient.multiply(this.learning_rate);
+        trans = Matrix.transpose(inputs);
+        delta = Matrix.multiply(gradient, trans);
+        // update
+        this.weights_ih.add(delta);
+        this.biase_i.add(gradient);
+      } else {
+        wtrans = Matrix.transpose(weights[i + 1]);
+        error = Matrix.multiply(wtrans, error);
+        gradient = Matrix.map(ho[ho.length - 1 - hoCount], dsigmoid);
+        gradient.hmultiply(error);
+        gradient.multiply(this.learning_rate);
+        trans = Matrix.transpose(ho[ho.length - 2 - hoCount]);
+        delta = Matrix.multiply(gradient, trans);
+        hoCount++;
+        // update
+        this.weightsHidden[this.hidden_layers - 1 - hoCount].add(delta);
+        this.biase_h[this.hidden_layers - 1 - hoCount].add(gradient);
+        weights[i].add(delta);
+      }
+    }
+    console.log('bitch this works');
+  }
 }
 
-let nn = new NeuralNetwork(2, [2, 4, 8], 2);
+let nn = new NeuralNetwork(2, [2], 1);
+// for (let i = 0; i < 10000; i++) {
+//   nn.train([1, 0], [1]);
+//   nn.train([0, 1], [1]);
+//   nn.train([1, 1], [0]);
+//   nn.train([0, 0], [0]);
+// }
 console.log(nn.pridict([1, 0]));
